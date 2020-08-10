2dcfdb958c7065f06b2f0648e074e940b026a3b4
xuyafeng <xuyafeng@taoche.com>
2020-08-10 11:42:31 +0800

    add test example

--- test.py
+++ test.py
@@ -10,10 +10,11 @@ from simplex_algorithm import SimplexAlgorithm
 #   x_i >= 0 (i = 1,2,3)
 
 #
-# A = np.array([[1, -2, 1], [-4, 1, 2], [2, 0, -1]])
-# B = np.array([11, 3, -1])
-# Z = np.array([-3, 1, 1])  # min
-# restrict = ['<=', '>=', '=']
+A = np.array([[1, -2, 1], [-4, 1, 2], [2, 0, -1]])
+B = np.array([11, 3, -1])
+Z = np.array([-3, 1, 1])  # min
+restrict = ['<=', '>=', '=']
+mode = 'min'
 
 
 # A = np.array([[-1, 2], [0, 1]])
@@ -21,11 +22,27 @@ from simplex_algorithm import SimplexAlgorithm
 # Z = np.array([1, 2]) # max
 # restrict = ['<=', '<=']
 
-A = np.array([[1, 2, 2, 1, 0], [3, 4, 1, 0, 1]])
-B = np.array([8, 7])
-Z = np.array([5, 2, 3, -1, 1])    # max
-restrict = ['=', '=']
+# A = np.array([[1, 2, 2, 1, 0], [3, 4, 1, 0, 1]])
+# B = np.array([8, 7])
+# Z = np.array([5, 2, 3, -1, 1])    # max
+# restrict = ['=', '=']
+
+# A = np.array([[-2, 2, 1, 0], [3, 1, 0, 1]])
+# B = np.array([4, 6])
+# Z = np.array([3, 1, 1, 1])    # min
+# restrict = ['=', '=']
+# mode = 'min'
+
+# A = np.array([[4, 5], [5, 2]])
+# B = np.array([100, 80])
+# Z = np.array([10, 5])    # max
+# restrict = ['<=', '<=']
+
+# A = np.array([[1, 0], [1, 2], [0, 1]])
+# B = np.array([5, 10, 4])
+# Z = np.array([1, 3])    # max
+# restrict = ['<=', '<=', '<=']
 
 if __name__ == '__main__':
-    optimal = SimplexAlgorithm(A, B, Z, 'max', restrict).run()
+    optimal = SimplexAlgorithm(A, B, Z, mode, restrict).run()
     print(optimal)
